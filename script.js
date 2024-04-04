d388577e779420b2a36c4fc97a9d5d2544f9ec79
NBS-Lewis Daveriel Masallo <lewisdaveriel.masallo@nbs.systems>
2024-04-04 09:12:59 +0800

    asd

--- script.js
+++ script.js
@@ -1,35 +1,53 @@
 document.addEventListener("DOMContentLoaded", function (arg) {
-    google.charts.load("current", { packages: ["corechart"] });
+    // google.charts.load("current", { packages: ["corechart"] });
 
-    google.charts.setOnLoadCallback(drawChart);
+    // google.charts.setOnLoadCallback(drawChart);
 
-    function drawChart() {
-        // Create the data table.
-        var data = new google.visualization.DataTable();
-        data.addColumn("string", "Topping");
-        data.addColumn("number", "Slices");
-        data.addRows([
-            ["Mushrooms", 3],
-            ["Onions", 1],
-            ["Olives", 1],
-            ["Zucchini", 1],
-            ["Pepperoni", 2],
-        ]);
+    // function drawChart() {
+    //     // Create the data table.
+    //     var data = new google.visualization.DataTable();
+    //     data.addColumn("string", "Topping");
+    //     data.addColumn("number", "Slices");
+    //     data.addRows([
+    //         ["Mushrooms", 3],
+    //         ["Onions", 1],
+    //         ["Olives", 1],
+    //         ["Zucchini", 1],
+    //         ["Pepperoni", 2],
+    //     ]);
 
-        // Set chart options
-        var options = {
-            title: "How Much Pizza I Ate Last Night",
-            width: 400,
-            height: 300,
-        };
+    //     // Set chart options
+    //     var options = {
+    //         title: "How Much Pizza I Ate Last Night",
+    //         width: 400,
+    //         height: 300,
+    //     };
 
-        var chart = new google.visualization.PieChart(
-            document.getElementById("chart_div")
-        );
-        chart.draw(data, options);
-    }
-    const error = "Error";
-    console.log("TEST");
+    //     var chart = new google.visualization.PieChart(
+    //         document.getElementById("chart_div")
+    //     );
+    //     chart.draw(data, options);
+    // }
+    const clickers = document.querySelectorAll(`.click`);
 
+    clickers.forEach(clicker => {
+        clicker.addEventListener('mousedown', (e) => {
+            clicker.classList.add('mousedown');
+            console.log(e.target.childNodes[0].data);
+        });
 
+        clicker.addEventListener('mouseleave', (e) => {
+            clicker.classList.remove('mousedown');
+        });
+
+        clicker.addEventListener('mouseup', (e) => {
+            clicker.classList.remove('mousedown');
+        });
+    });
+
+    const projects = document.querySelectorAll(`.project`);
+
+    projects.forEach(project => {
+
+    });
 });
